02e738e7a1751155bc0013a7049dc58e7c2a7717
onedrop20 <179625356+onedrop20@users.noreply.github.com>
2025-03-17 10:03:55 +0100

    Updates

---
+++ precache-manifest.2a1c91f37082201041cf87874adf966b.js
@@ -0,0 +1,146 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee65d812a758d1cdbee8a7e54043ea6a",
+    "url": "/My-portfolio-project-theone/index.html"
+  },
+  {
+    "revision": "1b1d036c3953803199af",
+    "url": "/My-portfolio-project-theone/static/css/2.a856fa37.chunk.css"
+  },
+  {
+    "revision": "4b011df5bb9eda7dd6d4",
+    "url": "/My-portfolio-project-theone/static/css/main.9cf68574.chunk.css"
+  },
+  {
+    "revision": "1b1d036c3953803199af",
+    "url": "/My-portfolio-project-theone/static/js/2.6c8305b1.chunk.js"
+  },
+  {
+    "revision": "a46f58956e3b48cc08a8",
+    "url": "/My-portfolio-project-theone/static/js/3.1629fdc3.chunk.js"
+  },
+  {
+    "revision": "4b011df5bb9eda7dd6d4",
+    "url": "/My-portfolio-project-theone/static/js/main.5e2c3c6b.chunk.js"
+  },
+  {
+    "revision": "1624d7276c4c5c0cf133",
+    "url": "/My-portfolio-project-theone/static/js/runtime~main.3ce749a3.js"
+  },
+  {
+    "revision": "5d10fdc258633fe53ff4e265f00ec95f",
+    "url": "/My-portfolio-project-theone/static/media/design-image-2.5d10fdc2.jpg"
+  },
+  {
+    "revision": "b92615c1c6145630e9f77ffa7ed991f9",
+    "url": "/My-portfolio-project-theone/static/media/design-image-3.b92615c1.jpg"
+  },
+  {
+    "revision": "b1f0c4acf575496c1eaf7c13371413ba",
+    "url": "/My-portfolio-project-theone/static/media/design.b1f0c4ac.jpg"
+  },
+  {
+    "revision": "54de569958cb9defa09bd658e53ef2c3",
+    "url": "/My-portfolio-project-theone/static/media/designimage.54de5699.jpg"
+  },
+  {
+    "revision": "15d54d142da2f2d6f2e90ed1d55121af",
+    "url": "/My-portfolio-project-theone/static/media/fa-brands-400.15d54d14.ttf"
+  },
+  {
+    "revision": "cbcf42b2e9228a8f5bad42717d8a88db",
+    "url": "/My-portfolio-project-theone/static/media/fa-brands-400.cbcf42b2.woff2"
+  },
+  {
+    "revision": "262525e2081311609d1fdab966c82bfc",
+    "url": "/My-portfolio-project-theone/static/media/fa-regular-400.262525e2.ttf"
+  },
+  {
+    "revision": "89672701a5874b80be27649e0494e354",
+    "url": "/My-portfolio-project-theone/static/media/fa-regular-400.89672701.woff2"
+  },
+  {
+    "revision": "269f971cec0d5dc864fe9ae080b19e23",
+    "url": "/My-portfolio-project-theone/static/media/fa-solid-900.269f971c.ttf"
+  },
+  {
+    "revision": "4a6591ab5460ae5cbff1ecbd6e52193a",
+    "url": "/My-portfolio-project-theone/static/media/fa-solid-900.4a6591ab.woff2"
+  },
+  {
+    "revision": "4ed293ceaca9b5b2d9cd74a477963fae",
+    "url": "/My-portfolio-project-theone/static/media/fa-v4compatibility.4ed293ce.ttf"
+  },
+  {
+    "revision": "a772da7bff216cd36214bc44165bba3e",
+    "url": "/My-portfolio-project-theone/static/media/fa-v4compatibility.a772da7b.woff2"
+  },
+  {
+    "revision": "83d56b38fcc40bdb2672aa4eca20ff12",
+    "url": "/My-portfolio-project-theone/static/media/image-1.83d56b38.jpg"
+  },
+  {
+    "revision": "b0c637e258d6f2934ed2d0df0017d9d7",
+    "url": "/My-portfolio-project-theone/static/media/image-10.b0c637e2.jpg"
+  },
+  {
+    "revision": "6e511ec7ad689a933c861cd6375d0a09",
+    "url": "/My-portfolio-project-theone/static/media/image-2.6e511ec7.jpg"
+  },
+  {
+    "revision": "fe25b79294ba7a84ce528e48aa15177b",
+    "url": "/My-portfolio-project-theone/static/media/image-3.fe25b792.jpg"
+  },
+  {
+    "revision": "206da34d309c876477e31c79d27a22bd",
+    "url": "/My-portfolio-project-theone/static/media/image-4.206da34d.jpg"
+  },
+  {
+    "revision": "e36c27277a0d7aab1ce924718dbf68c5",
+    "url": "/My-portfolio-project-theone/static/media/image-5.e36c2727.jpg"
+  },
+  {
+    "revision": "e36c27277a0d7aab1ce924718dbf68c5",
+    "url": "/My-portfolio-project-theone/static/media/image-7.e36c2727.jpg"
+  },
+  {
+    "revision": "23cb19276d00a23b444450bbaf1983c8",
+    "url": "/My-portfolio-project-theone/static/media/image-8.23cb1927.jpg"
+  },
+  {
+    "revision": "cc91e45bb862fe903c587b1059a5cfad",
+    "url": "/My-portfolio-project-theone/static/media/image-9.cc91e45b.jpg"
+  },
+  {
+    "revision": "d55dbe399e1b3d6b301a0fba46231f6c",
+    "url": "/My-portfolio-project-theone/static/media/my-resume.d55dbe39.PNG"
+  },
+  {
+    "revision": "5960c210cb2f9dd799282e04233b01ed",
+    "url": "/My-portfolio-project-theone/static/media/onedroplogo.5960c210.PNG"
+  },
+  {
+    "revision": "4b8393b6928e95558c0637cec6f48f3b",
+    "url": "/My-portfolio-project-theone/static/media/ui-image-7.4b8393b6.jpg"
+  },
+  {
+    "revision": "866e49601b09c2413c7c7c56080c0891",
+    "url": "/My-portfolio-project-theone/static/media/web-image-1.866e4960.jpg"
+  },
+  {
+    "revision": "8e8ac4cdb5639932587c441ee71c81f4",
+    "url": "/My-portfolio-project-theone/static/media/web-image-4.8e8ac4cd.jpg"
+  },
+  {
+    "revision": "b0895f1551047e90d31f714fa5a44625",
+    "url": "/My-portfolio-project-theone/static/media/web-image-5.b0895f15.jpg"
+  },
+  {
+    "revision": "7c96ff2514c4709bc45b50d673ea1aa3",
+    "url": "/My-portfolio-project-theone/static/media/web-image-6.7c96ff25.jpg"
+  },
+  {
+    "revision": "5eeb13ab3f51a4b4a75b940e2dce9e0e",
+    "url": "/My-portfolio-project-theone/static/media/web-image0.5eeb13ab.jpg"
+  }
+]);
